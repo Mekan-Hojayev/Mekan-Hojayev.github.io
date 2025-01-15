ceb1f9b91bbda1fa9b5526a019af98fb4d28f311
Mekan <mekanhojayev97@gmail.com>
2025-01-16 03:47:06 +0500

    changed achv

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 500); // Match this to the transition duration
     }, 3000); // 3 seconds delay before starting fade-out
 });
+
+function toggleAchievements() {
+    var extraAchievements = document.querySelector('.extra-achievements');
+    var button = document.querySelector('.toggle-button');
+
+    if (extraAchievements.style.display === "none") {
+        extraAchievements.style.display = "block";
+        button.textContent = "Show Less"; // Change button text to "Show Less"
+    } else {
+        extraAchievements.style.display = "none";
+        button.textContent = "Show More"; // Change button text to "Show More"
+    }
+}
